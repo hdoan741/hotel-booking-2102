247757e1199d09417bd23c9b01a9587a26dd5ee5
biyan <zhouby23@gmail.com>
2013-03-16 11:34:37 +0800

    Added .htaccess

--- CodeIgniter/application/config/config.php
+++ CodeIgniter/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8888/CodeIgniter/';
+$config['base_url']	= 'http://localhost/CodeIgniter';
 
 /*
 |--------------------------------------------------------------------------
