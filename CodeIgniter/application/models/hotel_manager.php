d14f451cd762985ffcca10753b3f75a51c8c3210
Tony Le <lvtien.dev@gmail.com>
2013-03-22 18:43:21 +0800

    update hotel model

---
+++ CodeIgniter/application/models/hotel_manager.php
@@ -0,0 +1,18 @@
+<?php
+class Hotel_manager extends CI_Model {
+
+	function __construct() {
+		parent::__construct();
+		$this->load->database();
+	}
+
+	function get_all_hotels() {
+		$query = $this->db->query("SELECT * FROM hotels");
+		return $query->result();
+	}
+
+	function delete_all_hotels() {
+		$query = $this->db->query("DELETE * FROM hotels");
+	}
+	
+}
