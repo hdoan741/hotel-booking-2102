f454f1479a9403149883034250af85bc8f204ab1
Hung Doan <hdoan741@gmail.com>
2013-03-28 20:30:47 +0800

    add a simple search form.

--- CodeIgniter/application/views/pages/home.php
+++ CodeIgniter/application/views/pages/home.php
@@ -3,7 +3,7 @@
 <div id="myCarousel" class="carousel slide">
   <div class="carousel-inner">
     <div class="item active">
-      <img src="http://twitter.github.com/bootstrap/assets/img/examples/slide-01.jpg">
+      <img src="/img/hotel1.JPG">
       <div class="container">
         <div class="carousel-caption">
           <h1>Example headline.</h1>
@@ -12,7 +12,7 @@
       </div>
     </div>
     <div class="item">
-      <img src="http://twitter.github.com/bootstrap/assets/img/examples/slide-02.jpg" alt="">
+      <img src="/img/hotel2.JPG">
       <div class="container">
         <div class="carousel-caption">
           <h1>Another example headline.</h1>
@@ -21,7 +21,7 @@
       </div>
     </div>
     <div class="item">
-      <img src="http://twitter.github.com/bootstrap/assets/img/examples/slide-03.jpg" alt="">
+      <img src="/img/hotel3.JPG">
       <div class="container">
         <div class="carousel-caption">
           <h1>One more for good measure.</h1>
@@ -33,4 +33,33 @@
   <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
   <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
 </div><!-- /.carousel -->
-<div class="container marketing">
+<div class="search-form">
+  <div class="search-header">
+    <h1>Book your hotel</h1>
+  </div>
+  <br/>
+  <form class="form-horizontal">
+    <div class="control-group">
+      <label class="control-label">Location</label>
+      <div class="controls">
+        <input type="text"></input>
+      </div>
+    </div>
+    <div class="control-group">
+      <label class="control-label">Number of Rooms</label>
+      <div class="controls">
+        <input type="text"></input>
+      </div>
+    </div>
+    <div class="control-group">
+      <label class="control-label">Number People</label>
+      <div class="controls">
+        <input type="text"></input>
+      </div>
+    </div>
+    <div class="form-actions">
+      <button class="btn btn-primary">Search!</button>
+    </div>
+  </form>
+</div>
+
