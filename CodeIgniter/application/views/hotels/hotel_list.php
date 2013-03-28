8905243a8e87434171f11291570f2ae8c69b99be
Carmen Quan <c2quan@uwaterloo.ca>
2013-03-28 22:39:35 +0800

    fixing hotel list

--- CodeIgniter/application/views/hotels/hotel_list.php
+++ CodeIgniter/application/views/hotels/hotel_list.php
@@ -6,12 +6,16 @@
 			<th>Hotel Code</th>
 			<th>Hotel Name</th>
 			<th>Hotel Location</th>
+			<th>&nbsp;</th>
+			<th>&nbsp;</th>
 		</tr>
 	<?php foreach ($hotels as $hotel):?>
 		<tr>
 			<td><?php echo $hotel->hotel_code;?></td>
 			<td><?php echo $hotel->name;?></td>
 			<td><?php echo $hotel->location;?></td>
+			<td><i class="icon-pencil"></i></td>
+			<td><a href="/hotels/delete/<?php echo $hotel->hotel_code; ?>"><i class="icon-trash"></i></a></td>
 		</tr>
 	<?php endforeach;?>
 	</table>
