6f3d87ace979257bb82db6270aad6fe67965a8b3
biyan <zhouby23@gmail.com>
2013-04-03 19:56:20 +0800

    Fixed id problem for booking

--- CodeIgniter/application/models/booking.php
+++ CodeIgniter/application/models/booking.php
@@ -11,7 +11,7 @@ class Booking extends CI_Model {
 	function __construct($attr=NULL) {
 		parent::__construct();
 		if($attr == NULL) return;
-		$this->id = $attr['id'];
+		if(array_key_exists('id', $attr)) $this->id = $attr['id'];
 		$this->start_date = $attr['start_date'];
 		$this->end_date = $attr['end_date'];
 		$this->customer = $attr['customer'];
