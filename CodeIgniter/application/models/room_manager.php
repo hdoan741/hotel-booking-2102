332ffe227d3e6e737492c94cca3cb28ce522d1de
biyan <zhouby23@gmail.com>
2013-04-03 21:51:23 +0800

    Modified search function in room_manager

--- CodeIgniter/application/models/room_manager.php
+++ CodeIgniter/application/models/room_manager.php
@@ -108,14 +108,24 @@ class Room_manager extends CI_Model {
 	}
 
 	function search($hotel_code, $start_date, $end_date) {
-                $format = 'SELECT r.room_code FROM bookings b, rooms r, room_booking rb'
-                        . 'WHERE r.room_code = rb.room_code'
-                        . 'AND b.id = rb.booking_id'
-			. 'AND r.hotel_code = \'%s\''
-                        . 'AND b.start_date > \'%s\''
-                        . 'AND b.end_date < \'%s\'';
-                $sql = sprintf($format, $hotel_code, $start_date, $end_date);
-                $query = $this->db->query($sql);
+                $format = 'SELECT r.room_code FROM rooms r'
+                        . 'WHERE r.room_code NOT IN ('
+                        .       'SELECT DISTINCT rb.room_code FROM room_booking rb, bookings b'
+                        .       'WHERE rb.booking_id = b.id'
+                        .       'AND ('
+                        .               '(b.start_date <= \'%s\' AND b.end_date >= \'%s\')'
+                        .               'OR'
+                        .               '(b.start_date <= \'%s\' AND b.end_date >= \'%s\')'
+                        .               'OR'
+                        .               '(b.start_date >= \'%s\' AND b.end_date <= \'%s\')'
+                        .               'OR'
+                        .               '(b.start_date <= \'%s\' AND b.end_date >= \'%s\')'
+                        .       ')'
+                        . ')'
+                        . 'AND r.hotel_code = \'%s\'';
+                $sql = sprintf($format, $end_date, $end_date, $start_date, $start_date, 
+			$start_date, $end_date, $start_date, $end_date, $hotel_code);
+		$query = $this->db->query($sql);
                 $rooms = array();
                 foreach ($query->result_array() as $row) {
                         array_push($rooms, get_room($row['room_code']));
