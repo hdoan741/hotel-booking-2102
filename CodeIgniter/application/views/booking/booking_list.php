aef8d3dcc7a2e2a0f296c60a751d6fafef7baebb
Tony Le <lvtien.dev@gmail.com>
2013-04-15 21:25:00 +0800

    update listing and deleting booking for admin

--- CodeIgniter/application/views/booking/booking_list.php
+++ CodeIgniter/application/views/booking/booking_list.php
@@ -5,21 +5,19 @@
 		<tr>
 			<th>Booking ID</th>
 			<th>Customer ID</th>
-			<th>Number of Adults</th>
-			<th>Number of Children</th>
 			<th>Arrival Date</th>
 			<th>Departure Date</th>
+			<th>&nbsp;</th>
 			<th>&nbsp;</th>
 		</tr>
 	<?php foreach ($bookings as $booking):?>
 		<tr>
 			<td><?php echo $booking->id;?></td>
 			<td><?php echo $booking->customer;?></td>
-			<td><?php echo $booking->num_adult;?></td>
-			<td><?php echo $booking->num_child;?></td>
 			<td><?php echo $booking->start_date;?></td>
 			<td><?php echo $booking->end_date;?></td>
-			<td><a href="/hotels/delete/<?php echo $booking->id; ?>"><i class="icon-trash"></i></a></td>
+			<td><a href="/booking/update/<?php echo $booking->id; ?>"><i class="icon-pencil"></i></a></td>
+			<td><a href="/booking/delete/<?php echo $booking->id; ?>"><i class="icon-trash"></i></a></td>
 		</tr>
 	<?php endforeach;?>
 	</table>
---
+++ CodeIgniter/application/views/booking/delete_booking_result.php
@@ -0,0 +1,14 @@
+<div class="container content">
+
+	<h1>A booking has been deleted</h1>
+
+	<p>
+		Booking ID: <?php echo $booking->id; ?>
+	</p>
+
+	<p>
+		Customer: <?php echo $booking->customer; ?>
+	</p>
+
+	<p><a href="/booking/list">Back to List</a></p>
+</div>
