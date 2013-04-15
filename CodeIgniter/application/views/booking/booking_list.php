1d982ab14f4c33dd994ea549536d3ac8ea887231
biyan <zhouby23@gmail.com>
2013-04-15 22:17:30 +0800

    update

--- CodeIgniter/application/views/booking/booking_list.php
+++ CodeIgniter/application/views/booking/booking_list.php
@@ -4,7 +4,6 @@
 	<table class='table table-striped'>
 		<tr>
 			<th>Booking ID</th>
-			<th>Customer ID</th>
 			<th>Arrival Date</th>
 			<th>Departure Date</th>
 			<th>&nbsp;</th>
@@ -13,7 +12,6 @@
 	<?php foreach ($bookings as $booking):?>
 		<tr>
 			<td><?php echo $booking->id;?></td>
-			<td><?php echo $booking->customer;?></td>
 			<td><?php echo $booking->start_date;?></td>
 			<td><?php echo $booking->end_date;?></td>
 			<td><a href="/booking/update/<?php echo $booking->id; ?>"><i class="icon-pencil"></i></a></td>
